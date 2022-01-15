405b42a8995063e6cbe57ec75c7ce1e8b08462e3
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-01-15 16:34:59 +0530

    - app update dialog support

---
+++ lib/app_update/model/app_update_config.dart
@@ -0,0 +1,19 @@
+class AppUpdateConfig {
+  String title;
+  String content;
+  String positiveButton;
+  String negativeButton;
+  String latestVersion;
+  String minimumVersion;
+  String appUrl;
+
+  AppUpdateConfig.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    content = json['content'];
+    positiveButton = json['positiveButton'];
+    negativeButton = json['negativeButton'];
+    latestVersion = json['latestVersion'];
+    minimumVersion = json['minimumVersion'];
+    appUrl = json['appUrl'] ?? '';
+  }
+}
---
+++ lib/app_update/service/app_update_helper.dart
@@ -0,0 +1,108 @@
+import 'package:crypto_khabar/app_update/model/app_update_config.dart';
+import 'package:crypto_khabar/dashboard/page/dashboard_page.dart';
+import 'package:crypto_khabar/shared/services/remote_config_service.dart';
+import 'package:crypto_khabar/shared/services/shared_pref_helper.dart';
+import 'package:crypto_khabar/shared/widget/view_utils.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AppUpdateHelper {
+  static final AppUpdateHelper _instance = AppUpdateHelper._();
+
+  factory AppUpdateHelper() {
+    return _instance;
+  }
+
+  AppUpdateHelper._();
+
+  static const String DefaultTitle = "अपडेट अलर्ट";
+  static const String DefaultContent =
+      "ऐप का नया वर्जन प्ले स्टोर पर उपलब्ध है।";
+
+  static const String DefaultUpdateButtonText = "अपडेट करें";
+  static const String DefaultIgnoreButtonTxt = "बाद में";
+
+  Future checkLatestUpdate() async {
+    try {
+      AppUpdateConfig config = await RemoteConfigService().getAppUpdateConfig();
+      if (config == null) {
+        print("AppUpdateHelper checkLatestUpdate AppUpdateConfig is null");
+        return;
+      }
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+      int appCurrentVersion = extractVersionFromString(packageInfo.version);
+      int minimumVersion = extractVersionFromString(config.minimumVersion);
+      int latestVersion = extractVersionFromString(config.latestVersion);
+
+      bool isForceUpdate = appCurrentVersion < minimumVersion;
+      bool shouldUpdate = appCurrentVersion < latestVersion;
+
+      if (shouldUpdate && globalContext != null) {
+        if (await shouldShowUpdateDialog()) {
+          showUpdateDialog(globalContext,
+              title: config?.title ?? DefaultTitle,
+              content: config?.content ?? DefaultContent,
+              positiveTextButton:
+                  config?.positiveButton ?? DefaultUpdateButtonText,
+              negativeTextButton:
+                  config?.negativeButton ?? DefaultIgnoreButtonTxt,
+              forceUpdate: isForceUpdate, positiveAction: () {
+            //Navigator.pop(globalContext);
+            launch(config.appUrl);
+          }, negativeAction: () {
+            saveUpdateCheckLaterTime();
+          }, onBackPress: (isDismiss) {
+            if (isDismiss) {
+              saveUpdateCheckLaterTime();
+            }
+          });
+        }
+      }
+    } catch (e) {
+      print("AppUpdateHelper checkLatestUpdate err:$e");
+    }
+  }
+
+  int extractVersionFromString(String versionStr) {
+    String currentVersion = versionStr
+        .replaceAll(".", "")
+        .replaceAll("-", "")
+        .replaceAll("-", "")
+        .replaceAll("+", "")
+        .trim();
+    int index = currentVersion.indexOf(RegExp(r'[a-zA-Z]'));
+    if (index > 0) {
+      currentVersion = currentVersion.substring(0, index);
+    }
+    try {
+      return int.parse(currentVersion);
+    } catch (e) {
+      return -1;
+    }
+  }
+
+  Future<bool> shouldShowUpdateDialog() async {
+    int lastUpdateCheckTime = await getUpdateCheckLaterTime();
+    if (lastUpdateCheckTime == -1) {
+      return true;
+    }
+    DateTime lastUpdateCheckDate =
+        DateTime.fromMillisecondsSinceEpoch(lastUpdateCheckTime);
+    int timeDiff = DateTime.now().difference(lastUpdateCheckDate).inHours;
+    return timeDiff >= 24;
+  }
+
+  Future saveUpdateCheckLaterTime() async {
+    await SharedPrefHelper().saveValue(
+        "updateCheckLaterTime", DateTime.now().millisecondsSinceEpoch);
+  }
+
+  Future<int> getUpdateCheckLaterTime() async {
+    String value = await SharedPrefHelper().getValue("updateCheckLaterTime");
+    if (value == null || value.isEmpty) {
+      return -1;
+    }
+    return int.parse(value);
+  }
+}
