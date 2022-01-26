511561ef6cb8142792df388548b7cbc90814242d
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-01-26 19:08:37 +0530

    - google ad integrated

---
+++ lib/ad/service/ad_helper.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:crypto_khabar/shared/services/remote_config_service.dart';
+
+class AdHelper {
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/2934735716';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static bool isAdEnabled() {
+    return RemoteConfigService().isAdEnabled();
+  }
+}
