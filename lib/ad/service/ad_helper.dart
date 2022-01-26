5bcf3c7187fc0b6b1c9361365171ef0695b4fba6
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-01-26 20:15:07 +0530

    - google ad id and unit id for release app updated

--- lib/ad/service/ad_helper.dart
+++ lib/ad/service/ad_helper.dart
@@ -5,9 +5,9 @@ import 'package:crypto_khabar/shared/services/remote_config_service.dart';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-8998311484189842/8306319133';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-3940256099942544/2934735716';//test
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
