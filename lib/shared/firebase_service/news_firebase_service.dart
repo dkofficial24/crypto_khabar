21c91ca93e2bbf85c8355c78867a1b9e99a9554d
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-03-18 15:57:02 +0530

    fix lazy load while carousel is enabled

--- lib/shared/firebase_service/news_firebase_service.dart
+++ lib/shared/firebase_service/news_firebase_service.dart
@@ -12,6 +12,7 @@ class NewsFirebaseService {
   }
 
   QueryDocumentSnapshot last;
+  QueryDocumentSnapshot featuredLast;
 
   Future<NewsItem> fetchNewsById(String id) async {
     CollectionReference newsRef = FirebaseFirestore.instance.collection("news");
@@ -52,13 +53,13 @@ class NewsFirebaseService {
 
   Future<List<NewsItem>> fetchFeaturedNews() async {
     List<NewsItem> newsItemList = [];
-    CollectionReference newsRef = FirebaseFirestore.instance.collection("featured_news");
+    CollectionReference newsRef = FirebaseFirestore.instance.collection("feature_news");
     QuerySnapshot data;
 
     data = await newsRef.orderBy('date', descending: true).limit(5).get();
 
     if (data != null && data.docs.length > 0) {
-      last = data.docs[data.docs.length - 1];
+      featuredLast = data.docs[data.docs.length - 1];
       data.docs.forEach((element) {
         if (element.exists) {
           newsItemList.add(NewsItem.fromJson(element.data()));
