54ed795deae72ec09467fab8707e2e527c4dd436
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-03-20 21:33:53 +0530

    - analytics added for tab change

--- lib/dashboard/page/dashboard_page.dart
+++ lib/dashboard/page/dashboard_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 BuildContext globalContext;
+
 class DashboardPage extends StatefulWidget {
   @override
   _DashboardPageState createState() => _DashboardPageState();
@@ -70,6 +71,17 @@ class _DashboardPageState extends State<DashboardPage> {
   void onTabSelect(int index) {
     setState(() {
       _selectedIndex = index;
+      String tabName = "";
+      if (_selectedIndex == 0) {
+        tabName = "tab_home";
+      } else if (_selectedIndex == 1) {
+        tabName = "tab_learn";
+      } else if (_selectedIndex == 2) {
+        tabName = "tab_market";
+      } else if (_selectedIndex == 3) {
+        tabName = "tab_setting";
+      }
+      FirebaseAnalytics.instance.logEvent(name: tabName);
     });
   }
 }
