6f376b298d30c472eb2f50df48d39b9b36bbfa7f
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-03-27 17:29:46 +0530

    - user count analytics added

--- lib/profile/service/profile_setting_service.dart
+++ lib/profile/service/profile_setting_service.dart
@@ -4,18 +4,22 @@ import 'package:crypto_khabar/constants.dart';
 import 'package:crypto_khabar/profile/feedback_info.dart';
 import 'package:crypto_khabar/shared/services/shared_pref_helper.dart';
 import 'package:crypto_khabar/utils/app_utils.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:uuid/uuid.dart';
 
 class ProfileSettingService {
-  String defaultDisclaimerMsg = "क्रिप्टो खबर द्वारा दी कोई भी जानकारी निवेश सलाह, वित्तीय सलाह, व्यापारिक सलाह या किसी अन्य प्रकार की सलाह नहीं है और क्रिप्टो खबर कभी क्रिप्टोकरेंसी खरीदने और बेचने की राय नहीं देता।क्रिप्टो मार्केट उच्च जोखिमों के अधीन है इसलिए कोई भी निवेश निर्णय लेने से पहले अच्छे से जानकारी हासिल कर लें।";
+  String defaultDisclaimerMsg =
+      "क्रिप्टो खबर द्वारा दी कोई भी जानकारी निवेश सलाह, वित्तीय सलाह, व्यापारिक सलाह या किसी अन्य प्रकार की सलाह नहीं है और क्रिप्टो खबर कभी क्रिप्टोकरेंसी खरीदने और बेचने की राय नहीं देता।क्रिप्टो मार्केट उच्च जोखिमों के अधीन है इसलिए कोई भी निवेश निर्णय लेने से पहले अच्छे से जानकारी हासिल कर लें।";
   String disclaimerMsg = "";
+
   ProfileSettingService._internal() {
     init();
   }
 
- String getDisclaimerMsg(){
-    if(disclaimerMsg == null || disclaimerMsg.isEmpty){
+  String getDisclaimerMsg() {
+    if (disclaimerMsg == null || disclaimerMsg.isEmpty) {
       disclaimerMsg = defaultDisclaimerMsg;
     }
     return disclaimerMsg;
@@ -32,6 +36,19 @@ class ProfileSettingService {
   bool get notificationStatus => _notificationStatus;
 
   init() {
+    try {
+      SharedPrefHelper().getValue("user_id").then((userId) {
+        if (userId == null) {
+          String uid = Uuid().v4();
+          SharedPrefHelper().saveValue("user_id", uid);
+          FirebaseAnalytics.instance.logEvent(name: "new_user");
+          incrementUserCount();
+        }
+      });
+    }catch(e){
+      print("Error ProfileSettingService init $e");
+    }
+
     disclaimerMsg = defaultDisclaimerMsg;
     getNotificationReceiveStatus().then((value) {
       _notificationStatus = value;
@@ -44,7 +61,7 @@ class ProfileSettingService {
     if (isManuallySet) {
       bool isDark = await isDarkTheme();
       BroadcastEvents().publish<bool>(ThemeChange, arguments: isDark);
-    }else {
+    } else {
       var brightness = SchedulerBinding.instance.window.platformBrightness;
       bool isDarkMode = brightness == Brightness.dark;
       setDarkTheme(isDarkMode);
@@ -87,6 +104,41 @@ class ProfileSettingService {
         .doc("${feedback.name}_$time")
         .set(feedback.toJson());
 
-   // print('published successfully !  ${feedback.toJson()}');
+    // print('published successfully !  ${feedback.toJson()}');
+  }
+
+  incrementUserCount() async {
+    final String fieldName = 'userCount';
+    String date =
+        AppUtils.formatOnlyDate(DateTime.now().millisecondsSinceEpoch);
+    final DocumentReference ref =
+        FirebaseFirestore.instance.collection('users').doc(date);
+    Future.delayed(Duration(seconds: 2)).then((value) async {
+      await getUserCount().then((int latestCount) async {
+        if (latestCount == 0) {
+          await ref.set({fieldName: 1});
+        } else {
+          await ref.update({
+            fieldName: latestCount + 1,
+          });
+        }
+      });
+    });
+  }
+
+  Future<int> getUserCount() async {
+    final String fieldName = 'userCount';
+    String date =
+        AppUtils.formatOnlyDate(DateTime.now().millisecondsSinceEpoch);
+    final DocumentReference ref =
+        FirebaseFirestore.instance.collection('users').doc(date);
+    DocumentSnapshot snap = await ref.get();
+    int itemCount;
+    try {
+      itemCount = snap[fieldName] ?? 0;
+    } catch (e) {
+      itemCount = 0;
+    }
+    return itemCount;
   }
 }
