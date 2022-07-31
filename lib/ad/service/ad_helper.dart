d6dfeefde68bd36b214fcda5fda726d3ee642431
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2022-07-31 19:38:17 +0530

    - testing code reverted

--- lib/ad/service/ad_helper.dart
+++ lib/ad/service/ad_helper.dart
@@ -6,9 +6,9 @@ import 'package:crypto_khabar/shared/services/remote_config_service.dart';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-8998311484189842/8306319133';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';//test
+      return 'ca-app-pub-3940256099942544/2934735716';
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
