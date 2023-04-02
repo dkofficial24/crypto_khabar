d167346d37d19b32a58f296d3685b25d457bb95c
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2023-04-02 20:23:32 +0530

    unit test demo added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,14 @@
 
 // import 'package:crypto_khabar/main.dart';
 // import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test(
+      "TestDemo",
+      () => () {
+            expect(1, 1);
+          });
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
   //   await tester.pumpWidget(MyApp());
