a0a145938a769ac3b42b606bbad6ce3985cde47d
yogeshkumar24 <yogesh.kumar.official97@gmail.com>
2023-04-02 13:59:04 +0530

    - removed webview

--- lib/shared/page/web_view_page.dart
+++ lib/shared/page/web_view_page.dart
@@ -1,6 +1,5 @@
-import 'package:crypto_khabar/utils/string_const.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+//import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewPage extends StatefulWidget {
   @override
@@ -21,30 +20,30 @@ class WebViewPageState extends State<WebViewPage> {
     if (link == null) {
       link = ModalRoute.of(context).settings.arguments;
     }
-
-    return Scaffold(
-      appBar: AppBar(title: Text(StringConst.appName),),
-      body: Center(
-        child: IndexedStack(
-          index:index,
-          children: [
-            WebView(
-              initialUrl: link,
-              onPageFinished: (_) {
-                setState(() {
-                  index = 0;
-                });
-              },
-              onPageStarted: (_){
-                setState(() {
-                  index = 1;
-                });
-              },
-            ),
-            Center(child: CircularProgressIndicator(),)
-          ],
-        ),
-      ),
-    );
+    return Container();
+    // return Scaffold(
+    //   appBar: AppBar(title: Text(StringConst.appName),),
+    //   body: Center(
+    //     child: IndexedStack(
+    //       index:index,
+    //       children: [
+    //         WebView(
+    //           initialUrl: link,
+    //           onPageFinished: (_) {
+    //             setState(() {
+    //               index = 0;
+    //             });
+    //           },
+    //           onPageStarted: (_){
+    //             setState(() {
+    //               index = 1;
+    //             });
+    //           },
+    //         ),
+    //         Center(child: CircularProgressIndicator(),)
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
